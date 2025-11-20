356ae5a8fc2333cfc3c0c28257dc461d64692d43
hernanspc <58519791+hernanspc@users.noreply.github.com>
2025-11-20 13:33:36 -0500

    FIX: screen login , loading ,

---
+++ lib/src/config/app_router.dart
@@ -0,0 +1,30 @@
+import 'package:delivery_app/src/pages/home/home_screen.dart';
+import 'package:delivery_app/src/pages/login/login_screen.dart';
+import 'package:go_router/go_router.dart';
+import '../pages/loading/loading_screen.dart';
+
+final appRouter = GoRouter(
+  initialLocation: '/loading_screen',
+  routes: [
+    GoRoute(
+      path: '/loading_screen',
+      name: 'loading_screen',
+      builder: (context, state) => LoadingScreen(),
+    ),
+    GoRoute(
+      path: '/login_screen',
+      name: 'login_screen',
+      builder: (context, state) => LoginScreen(),
+    ),
+    GoRoute(
+      path: '/home_screen',
+      name: 'home_screen',
+      builder: (context, state) => HomeScreen(),
+    ),
+  ],
+  redirect: (context, state) {
+    final uri = state.uri;
+    print('🟡 screen: $uri');
+    return null;
+  },
+);
---
+++ lib/src/pages/pages.dart
@@ -0,0 +1,1 @@
+export 'package:delivery_app/src/pages/pages.dart';
